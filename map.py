b8899d0b7ad8d59453c386fc4b476e3296f69c85
Vinnie117 <hhang@hotmail.de>
2025-12-04 09:49:45 +0100

    centered A4

--- map.py
+++ map.py
@@ -1,23 +1,48 @@
 import osmnx as ox
+import matplotlib.pyplot as plt
 
+# --- SETTINGS ---
 ox.settings.use_cache = True
 ox.settings.log_console = True
 
 place_name = "Berlin, Germany"
+margin_mm = 20                             # margin around the map
+a4_width_mm = 210
+a4_height_mm = 297
 
-# Download the street network
+# Convert mm → inches for Matplotlib
+mm_to_inch = 1 / 25.4
+fig_width_in = a4_width_mm * mm_to_inch
+fig_height_in = a4_height_mm * mm_to_inch
+margin_in = margin_mm * mm_to_inch
+
+# Compute normalized figure margins
+left = right = margin_in / fig_width_in
+bottom = top = margin_in / fig_height_in
+
+# --- DOWNLOAD GRAPH ---
 G = ox.graph_from_place(place_name, network_type="drive")
 
-# Plot and save as SVG
-fig, ax = ox.plot_graph(
+# --- CREATE FIGURE ---
+fig = plt.figure(figsize=(fig_width_in, fig_height_in))
+
+# Add axes with margins applied
+ax = fig.add_axes([left, bottom, 1 - left - right, 1 - top - bottom])
+
+# --- DRAW GRAPH ---
+ox.plot_graph(
     G,
+    ax=ax,
+    show=False,
+    close=False,
     bgcolor="white",
     node_size=0,
     edge_color="black",
-    edge_linewidth=0.5,
-    save=True,
-    filepath="berlin.svg",   # extension determines format
-    dpi=300
+    edge_linewidth=0.5
 )
 
-print("Saved as berlin.svg")
+# --- SAVE AS SVG ---
+fig.savefig("berlin_A4_centered.svg", format="svg")
+plt.close(fig)
+
+print("Saved as berlin_A4_centered.svg")
